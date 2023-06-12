c85f985a46f901652da30ef60ab60f85cb60760a
Jeon Hyewon <107667966+hyennii@users.noreply.github.com>
2023-06-12 18:20:33 +0900

    Create data.js

---
+++ codingapple/shopping-mall/03/data.js
@@ -0,0 +1,24 @@
+let data = [
+    {
+      id : 0,
+      title : "댕 노즈워크",
+      content : "Born in France",
+      price : 25000
+    },
+  
+    {
+      id : 1,
+      title : "마약 삑삑이",
+      content : "Born in Seoul",
+      price : 2000
+    },
+  
+    {
+      id : 2,
+      title : "비가와도 산책할래",
+      content : "Born in the States",
+      price : 30000
+    }
+  ] 
+
+  export default data;
