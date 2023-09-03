bdcd781b7549b727213f18867e0d153f896cb113
Jeon Hyewon <107667966+hyennii@users.noreply.github.com>
2023-09-03 19:18:31 +0900

    코드 수정

--- codingapple/shopping-mall/15/store.js
+++ codingapple/shopping-mall/15/store.js
@@ -1,7 +1,5 @@
-import {configureStore} from '@reduxJs/toolit'
+import { configureStore } from '@reduxjs/toolkit'
 
 export default configureStore({
-    reducer: {
-        
-    }
-})
+  reducer: { }
+}) 
